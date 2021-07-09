1f8f451d528ba56b09fb50703baf6795e0d12006
Li Shengyan <49756674+ZhugeKongan@users.noreply.github.com>
2021-07-09 14:39:22 +0800

    Add files via upload

---
+++ train.py
@@ -0,0 +1,257 @@
+#引入必须的包
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+data_path='./dataset'#数据保存路径
+
+#数据的预处理操作
+training_transform=transforms.Compose([
+            transforms.RandomCrop(32, padding=4),#数据增广
+            transforms.RandomHorizontalFlip(),#数据增广
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.4914, 0.4822, 0.4465],std=[0.2471, 0.2435, 0.2616]),#数据归一化
+        ])
+validation_transforms=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.4914, 0.4822, 0.4465],std=[0.2471, 0.2435, 0.2616]),
+        ])
+
+#训练集
+train_dataset = torchvision.datasets.CIFAR100(root=data_path,#数据下载和加载
+                                             train=True,
+                                             transform=training_transform,
+                                             download=True)#若已下载改为False
+#测试集
+val_dataset = torchvision.datasets.CIFAR100(root=data_path,
+                                            train=False,
+                                            transform=validation_transforms,
+                                            download=True)
+x,y= train_dataset[0]
+print(x.size(),y)
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def Conv1(in_planes, places, stride=2):
+    return nn.Sequential(
+        nn.Conv2d(in_channels=in_planes, out_channels=places, kernel_size=3, stride=1, padding=1, bias=False),
+        nn.BatchNorm2d(places),
+        nn.ReLU(inplace=True),
+        # nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+    )
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3,
+                               stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.shortcut = nn.Sequential()
+        # 经过处理后的x要与x的维度相同(尺寸和深度)
+        # 如果不相同，需要添加卷积+BN来变换为同一维度
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion * planes)
+            )
+
+
+    def forward(self, x):
+        # print(x.size())
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+
+        out += self.shortcut(x)
+        #attention block
+
+        out = F.relu(out)
+        return out
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+        self.conv1 = Conv1(in_planes=3, places=64)
+
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.classifer = nn.Linear(512*block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size()[0], -1)  # 4, 2048
+        x = self.classifer(x)
+        return x
+
+def ResNet18(**kwargs):
+    return ResNet(BasicBlock, [2, 2, 2, 2],**kwargs)
+
+net = ResNet18(num_classes=10).cuda()
+# y = net(torch.randn(1, 3, 32, 32))
+# print(y.size())
+
+import os
+import sys
+import argparse
+import time
+from datetime import datetime
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+
+from torch.optim.lr_scheduler import _LRScheduler
+class WarmUpLR(_LRScheduler):
+    """warmup_training learning rate scheduler
+    Args:
+        optimizer: optimzier(e.g. SGD)
+        total_iters: totoal_iters of warmup phase
+    """
+    def __init__(self, optimizer, total_iters, last_epoch=-1):
+
+        self.total_iters = total_iters
+        super().__init__(optimizer, last_epoch)
+
+    def get_lr(self):
+        """we will use the first m batches, and set the learning
+        rate to base_lr * m / total_iters
+        """
+        return [base_lr * self.last_epoch / (self.total_iters + 1e-8) for base_lr in self.base_lrs]
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""  # [128, 10],128
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)  # [128, 5],indices
+
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))  # 5,128
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
+        wrong_k = batch_size - correct_k
+        res.append(wrong_k.mul_(100.0 / batch_size))
+
+    return res
+#超参数
+warm=1
+epoch=160
+batch_size=128
+loss_function = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.1, momentum=0.9, weight_decay=1e-4)
+train_scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[80,120], gamma=0.1) #learning rate decay
+
+from torch.utils.data import  DataLoader
+trainloader = DataLoader(train_dataset, batch_size, shuffle=True, num_workers=5, pin_memory=True)
+valloader = DataLoader(val_dataset, batch_size, shuffle=False, num_workers=5, pin_memory=True)
+iter_per_epoch = len(trainloader)
+warmup_scheduler = WarmUpLR(optimizer, iter_per_epoch * warm)
+
+
+def train(trainloader, model, criterion, optimizer, epoch):
+
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    model.train()
+    for i, (input, target) in enumerate(trainloader):
+        # measure data loading time
+
+        input, target = input.cuda(), target.cuda()
+
+        # compute output
+        output = model(input)
+        loss = criterion(output, target)
+
+        # measure accuracy and record loss
+        prec = accuracy(output, target)[0]
+        losses.update(loss.item(), input.size(0))
+        top1.update(prec.item(), input.size(0))
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    return top1.avg
+
+def validate(val_loader, model, criterion):
+
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    model.eval()
+    with torch.no_grad():
+        for i, (input, target) in enumerate(val_loader):
+            input, target = input.cuda(), target.cuda()
+
+            # compute output
+            output = model(input)
+            loss = criterion(output, target)
+
+            # measure accuracy and record loss
+            prec = accuracy(output, target)[0]
+            losses.update(loss.item(), input.size(0))
+            top1.update(prec.item(), input.size(0))
+
+    print(' * Prec {top1.avg:.3f}% '.format(top1=top1))
+
+    return top1.avg
+best_prec = 0
+for e in range(epoch):
+        train_scheduler.step( e)
+
+        # train for one epoch
+        train(trainloader, net, loss_function, optimizer, e)
+
+        # evaluate on test set
+        prec = validate(valloader, net, loss_function)
+
+        # remember best precision and save checkpoint
+        is_best = prec > best_prec
+        best_prec = max(prec,best_prec)
+print(best_prec)
